d8ad7f67fadffadeb75f672be2d05ce2fc3e47f5
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-17 22:42:50 +0600

    pythonString method ord('char') and chr(integer)

--- stringIntro.py
+++ stringIntro.py
@@ -3,4 +3,17 @@ String: a string is a sequence of unicode charecter.
 a=>unicode value assign in computer memmory=> integer value for
 unicode=> computer understood binary.
 a=>unicode=>integer=>binary
+ord('char') method char er unicode return kore.
+chr(integer) method unicode value niye charecter return kore
+
 '''
+info="MSA is will be a conquerer"
+details="""
+MSA is a software engineer. He got HSC certificate from GSC.
+He currently studied on NSTU.
+"""
+# print(details)
+valueInt=ord('A') # 65 is a unicode value for capital A, 97 for
+# small a
+valueChar=chr(97) # a, chr method unicode niye char return kore
+print(valueChar)
