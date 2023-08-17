40f028b4be0377923e7774d67e9754380e770d90
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-17 22:28:07 +0600

    python string intro

---
+++ stringIntro.py
@@ -0,0 +1,6 @@
+'''
+String: a string is a sequence of unicode charecter.
+a=>unicode value assign in computer memmory=> integer value for
+unicode=> computer understood binary.
+a=>unicode=>integer=>binary
+'''
